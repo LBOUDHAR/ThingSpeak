0e0832ee01dc159ee7eace718006225db89d9262
Lies BOUDHAR <37364280+LBOUDHAR@users.noreply.github.com>
2024-01-12 23:13:43 +0100

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.21"
+}
+
+android {
+    namespace = "com.example.thingspeakviewer2023bl"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.thingspeakviewer2023bl"
+        minSdk = 29
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    implementation ("com.squareup.retrofit:retrofit:2.0.0-beta2")
+    implementation ("com.squareup.retrofit:converter-gson:2.0.0-beta2")
+    implementation ("com.squareup.okhttp:okhttp:2.5.0")
+
+    implementation ("io.github.muddz:styleabletoast:2.4.0")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
